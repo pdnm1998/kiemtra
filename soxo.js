c4ee8546f6148b0c613b7be120b81eeb52f0db5c
nhatminh <nkocvjp92@gmail.com>
2021-06-15 20:53:58 +0700

    nhatminh

--- soxo.js
+++ soxo.js
@@ -5,26 +5,24 @@ let num
 let machineNum = parseInt(Math.random() * 10)
 console.log(+machineNum);
 
-for (let i = 0; i < 3; i++) {
-    $btn[i].addEventListener("click", function(e) {
-        console.log($fm.dl.value.trim());
-        $fm.addEventListener("submit", function(e) {
-            e.preventDefault();
-            // Truy cap toi input co name la dl
-            if ($fm.dl.value.trim() === machineNum) {
-                function dudoan(a) {
-                    alert(a);
-                }
-                dudoan("chúc mung ban !!!")
-                return;
-            } else {
-                function dudoan(a) {
-                    alert(a);
-                }
-                dudoan("chúc ban may man lan sau !!!")
+$btn[i].addEventListener("click", function(e) {
+    console.log($fm.dl.value.trim());
+    $fm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        // Truy cap toi input co name la dl
+        if ($fm.dl.value.trim() === machineNum) {
+            function dudoan(a) {
+                alert(a);
             }
-
-        })
+            dudoan("chúc mung ban !!!")
+            return;
+        } else {
+            function dudoan(a) {
+                alert(a);
+            }
+            dudoan("chúc ban may man lan sau !!!")
+        }
 
     })
-}
+
+})
