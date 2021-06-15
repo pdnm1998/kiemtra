8b014dfe11f376489d7c15b4618ab3d928ac7c96
nhatminh <nkocvjp92@gmail.com>
2021-06-15 20:50:42 +0700

    nhatminh

---
+++ bai2.js
@@ -0,0 +1,15 @@
+let a = [];
+let b = [];
+
+function merge2String(x, y) {
+    x = a;
+    console.log(x);
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < b.length; j++) {
+            //  
+        }
+    }
+    return x;
+}
+let m = merge2String("minh", "tung");
+console.log(m);
